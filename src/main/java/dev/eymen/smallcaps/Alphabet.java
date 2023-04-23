1d8571a35b912722f5c99d84326a0d1bbf082d7b
Jakub Šulc <59335201+RETAMROVEC@users.noreply.github.com>
2023-04-23 20:16:46 +0200

    Create Alphabet.java

---
+++ src/main/java/dev/eymen/smallcaps/Alphabet.java
@@ -0,0 +1,23 @@
+package dev.eymen.smallcaps;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Alphabet {
+
+	private static final String ORIGINAL_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String REPLACEMENT_ALPHABET = "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ";
+
+	public static @NotNull String convert(@NotNull String input) {
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			char currentChar = input.charAt(i);
+			int index = ORIGINAL_ALPHABET.indexOf(currentChar);
+			if (index >= 0) {
+				output.append(REPLACEMENT_ALPHABET.charAt(index));
+			} else {
+				output.append(currentChar);
+			}
+		}
+		return output.toString();
+	}
+}
